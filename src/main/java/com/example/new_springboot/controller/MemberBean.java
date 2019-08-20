450b188ee90573ce9b1d7c4840a9042bc2ba7cda
JaewoongByeon <jwbyeon330@gmail.com>
2019-08-20 15:22:50 +0900

    0.7

---
+++ src/main/java/com/example/new_springboot/controller/MemberBean.java
@@ -0,0 +1,21 @@
+package com.example.new_springboot.controller;
+
+public class MemberBean {
+    public String EMAIL;
+
+    public String getEMAIL() {
+        return EMAIL;
+    }
+
+    public void setEMAIL() {
+    }
+
+    public String NAME;
+
+    public String getNANE() {
+        return NAME;
+    }
+
+    public void setNAME() {
+    }
+}
--- src/main/java/com/example/new_springboot/controller/MemberController.java
+++ src/main/java/com/example/new_springboot/controller/MemberController.java
@@ -1,13 +1,25 @@
 package com.example.new_springboot.controller;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping(value = "/member")
 public class MemberController {
     @RequestMapping(value = { "/login", "/logout", "/signup" }, method = { RequestMethod.GET, RequestMethod.POST })
     public void actionMethod() {
+    }
+
+    @RequestMapping(value = "/view")
+    public ModelAndView read(@RequestParam Map<String, Object> paramMap, ModelAndView modelAndView) {
+        String viewName = "member/view";
+        modelAndView.setViewName(viewName);
+        modelAndView.addObject("paramMap", paramMap);
+        return modelAndView;
     }
 }
