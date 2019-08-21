3d0168d86b05aabf4eb711116e47dab97bd52ca8
JaewoongByeon <jwbyeon330@gmail.com>
2019-08-21 10:32:17 +0900

    0.9

--- src/main/java/com/example/new_springboot/controller/CommonCodeController.java
+++ src/main/java/com/example/new_springboot/controller/CommonCodeController.java
@@ -1,5 +1,7 @@
 package com.example.new_springboot.controller;
 
+import java.awt.List;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -21,13 +23,29 @@ public class CommonCodeController {
     public ModelAndView edit(@RequestParam Map<String, Object> paramMap, @PathVariable String action,
             ModelAndView modelandView) {
         String viewName = "/commonCode/";
+        ArrayList<Object> resultList = new ArrayList<Object>();
         Map<String, Object> resultMap = new HashMap<String, Object>();
         if ("view".equals(action)) {
             viewName = viewName + action;
             resultMap = paramMap;
+            modelandView.setViewName(viewName);
+            modelandView.addObject("resultMap", resultMap);
+        } else if ("search".equals(action)) {
+            Map<String, Object> data01 = new HashMap<String, Object>();
+            data01.put("QUERY", paramMap.get("QUERY"));
+            data01.put("RULE", paramMap.get("RULE"));
+            Map<String, Object> data02 = new HashMap<String, Object>();
+            data02.put("NAME", "name 01");
+            data02.put("COMMON_CODE_ID", "490293232849");
+            data02.put("DESCRIPTION", "anything DESCRIPTION");
+            viewName = viewName + action;
+            modelandView.setViewName(viewName);
+            resultList.add(data02);
+            modelandView.addObject("resultList1", data01);
+            modelandView.addObject("resultList2", resultList);
+        } else {
+            modelandView.setViewName(viewName);
         }
-        modelandView.setViewName(viewName);
-        modelandView.addObject("resultMap", resultMap);
         return modelandView;
     }
 }
